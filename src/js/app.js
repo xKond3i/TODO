a3c7458c59b54af37ff22eb5b0bf141c3a255093
xKond3i <ceg.kond@gmail.com>
2022-06-04 00:29:21 +0200

    animations

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,7 @@ const todo_container    = document.querySelector('[data-todo-container]')
 const todo_list         = todo_container.querySelector('[data-todo-list]')
 const todo_add_btn      = todo_container.querySelector('[data-todo-add-btn]')
 const todo_add_content  = todo_container.querySelector('[data-todo-add-content]')
-const todo_del_all_btn  = todo_container.querySelector('[data-todo-delete-all-btn]')
+const todo_del_all_btn  = document.querySelector('[data-todo-delete-all-btn]')
 const todo_none_left    = todo_list.querySelector('[data-todo-info]')
 
 
@@ -60,6 +60,10 @@ function addTodoElement(data) {
         saveToLocalStorage()
     })
 
+    new_todo.addEventListener('animationend', () => {
+        new_todo.classList.remove('slide-in')
+    }, {once: true})
+
     // Handle deletion of todo element
     let new_todo_del_btn = new_todo.querySelector('[data-todo-delete-btn]')
     new_todo_del_btn.addEventListener('click', () => deleteTodo(new_todo))
@@ -91,7 +95,13 @@ function addTodo(title, done = false) {
 // Handle new entry input
 function validTodo() {
     // Todo has to have a title
-    if (todo_add_content.value.length == 0) return false
+    if (todo_add_content.value.length == 0) {
+        todo_container.classList.add('invalid-shake')
+        todo_container.addEventListener('animationend', () => {
+            todo_container.classList.remove('invalid-shake')
+        }, {once: true})
+        return false
+    }
     return true
 }
 
@@ -112,15 +122,19 @@ todo_add_content.addEventListener('keypress', (e) => { if (e.key == 'Enter') add
 
 // Delete single todo both from array and as an element
 function deleteTodo(todo_element) {
-    let todo_entry_elements = [...todo_list.querySelectorAll('[data-todo-entry]')]
-    todos.splice(todo_entry_elements.indexOf(todo_element), 1)
-    todo_list.removeChild(todo_element)
+    // Slide out animation
+    todo_element.classList.add('slide-out')
+    todo_element.addEventListener('animationend', () => {
+        let todo_entry_elements = [...todo_list.querySelectorAll('[data-todo-entry]')]
+        todos.splice(todo_entry_elements.indexOf(todo_element), 1)
+        todo_list.removeChild(todo_element)
 
-    // Update localStorage
-    saveToLocalStorage()
+        // Update localStorage
+        saveToLocalStorage()
 
-    toggleEmptyNotification() // None left notification
-    updateAddContentInput() // Increasing paper size on input effect
+        toggleEmptyNotification() // None left notification
+        updateAddContentInput() // Increasing paper size on input effect
+    }, {once: true})
 }
 
 // Delete all todos both from array and as an element
@@ -143,8 +157,27 @@ function toggleEmptyNotification() {
 
 
 // DETAILS
-// Increasing paper size on input effect
-// Monospace font would look the best
 const todo_add_content_input = document.querySelector('.__todo_add_content')
-const updateAddContentInput = () => { todo_add_content_input.setAttribute('size', todo_add_content_input.value.length) }
+
+var placeholder_txt = 'Type your todo here...'
+var letter_interval = 50
+var pause_typing_anim = false
+
+// Setup input font size to work with typing effect
+todo_add_content_input.setAttribute('size', placeholder_txt.length)
+
+// Increasing paper size on input effect
+const updateAddContentInput = () => { 
+    let len = todo_add_content_input.value.length
+    pause_typing_anim = len > 0
+    todo_add_content_input.setAttribute('size', len > placeholder_txt.length ? len - 1 : placeholder_txt.length)
+}
 todo_add_content_input.addEventListener('input', updateAddContentInput)
+
+// Typing text effect
+const addContentInputPlaceholderAnimation = setInterval( () => {
+    todo_add_content_input.placeholder = ''
+    for (let i = 0; i < placeholder_txt.length; i++) {
+        if (!pause_typing_anim) setTimeout(() => { todo_add_content_input.placeholder += placeholder_txt.charAt(i)}, i * letter_interval)
+    }
+}, placeholder_txt.length * letter_interval + 1000)
